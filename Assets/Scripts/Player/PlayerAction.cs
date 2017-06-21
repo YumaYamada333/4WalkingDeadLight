db9cb909af28a57753cd8c318e881d0d9a29c69f
sakuma <soramamenatan@gmail.com>
2017-06-21 15:20:57 +0900

    カウントのバグ修正

--- Assets/Scripts/Player/PlayerAction.cs
+++ Assets/Scripts/Player/PlayerAction.cs
@@ -20,7 +20,7 @@ static class Constants
     public const int SuperAttack = 3;  //superattack
     public const int MaxEnemy    = 4;  //敵の数
     public const int MaxJumpPow  = 5;  //最大のジャンプ力
-    public const int MaxAnimation= 6;  //最大のアニメーションの数
+    public const int MaxAnimation= 7;  //最大のアニメーションの数
     public const int MaxTime     = 10; //最大時間
     public const int MoveCount   = 60; //移動エフェクトのループ再生する間隔
 
@@ -29,7 +29,7 @@ static class Constants
     public const float MassDistance = 2.2f; //マスの距離
 }
 //アニメーション
-enum ANIMATION { MOVE, JUMP, ATTACK, COUNT, OVER };
+enum ANIMATION { MOVE, JUMP, ATTACK, COUNT, OVER, };
 //パーティクル
 enum PARTICLE { NONE, MOVE,ATTACK,DAMAGE,LANDING,WATER,POISON};
 public class PlayerAction : MonoBehaviour
@@ -316,7 +316,8 @@ public class PlayerAction : MonoBehaviour
     public bool IsIdle()
     {
         //待機中の場合
-        if (animationFlag[(int)ANIMATION.MOVE] == false && animationFlag[(int)ANIMATION.JUMP] == false && animationFlag[(int)ANIMATION.ATTACK] == false)
+        if (animationFlag[(int)ANIMATION.MOVE] == false && animationFlag[(int)ANIMATION.JUMP] == false && animationFlag[(int)ANIMATION.ATTACK] == false
+            && animationFlag[(int)ANIMATION.COUNT] == false)
         {
             idleFlag = true;
         }
@@ -363,19 +364,12 @@ public class PlayerAction : MonoBehaviour
                     break;
                 //attack
                 case (int)ANIMATION.ATTACK:
-                //case (int)ANIMATION.COUNT:
-                    //移動しない
-                    middlePosition = new Vector3(transform.position.x, middlePosition.y, 0);
-                    //移動しない
-                    endPosition = new Vector3(transform.position.x, endPosition.y, 0);
-                    break;
                 case (int)ANIMATION.COUNT:
                     //移動しない
                     middlePosition = new Vector3(transform.position.x, middlePosition.y, 0);
                     //移動しない
                     endPosition = new Vector3(transform.position.x, endPosition.y, 0);
                     break;
-
             }
 
         }
